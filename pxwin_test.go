6334240cc80acab0bdb8d2a83f4aae3cd926d8c8
Yuchen Wang <yulon@me.com>
2015-11-25 05:40:54 +0100

    Update paint demo

--- pxwin_test.go
+++ pxwin_test.go
@@ -2,18 +2,16 @@ package pxwin
 
 import (
 	"testing"
+	"bytes"
 	//"fmt"
 )
 
 func TestWindow(*testing.T) {
 	Main(func() {
 		w := New()
-		p := make([]byte, 500 * 500 * 4)
-		for i := 0; i < len(p); i++ {
-			p[i] = 255
-		}
 		w.On(EventPaint, func(param ...int){
-			w.Paint(p)
+			r := w.Rect()
+			w.Paint(bytes.Repeat([]byte{155}, r.Width * r.Height * 4))
 		})
 	})
 }
